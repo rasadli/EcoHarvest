44fd81ae6bbdff7b3e8e5d7753b5c34a01e90a04
Rashad014 <119725193+Rashad014@users.noreply.github.com>
2023-01-28 13:55:51 +0400

    Add files via upload
    test

---
+++ customs.js
@@ -0,0 +1,43 @@
+Fruits.addEventListener("click",function(){
+    document.querySelectorAll(".variant").forEach(item=>{
+        item.classList.remove("variant1")
+    })
+    this.classList.add("variant1")
+    Pimg1.src="http://ecoharvest.farm/img/NewCategory/rich%20may%20peach%20.png"
+    Pimg2.src="http://ecoharvest.farm/img/NewCategory/summer%20sweet%20peach.png"
+    Pimg3.src="http://ecoharvest.farm/img/NewCategory/white-peach%20.png"
+    Pimg4.src="http://ecoharvest.farm/img/NewCategory/peach%20flat.png "
+})
+
+Vegetables.addEventListener("click",function(){
+    document.querySelectorAll(".variant").forEach(item=>{
+        item.classList.remove("variant1")
+    })
+    this.classList.add("variant1")
+    Pimg1.src="http://ecoharvest.farm/img/NewCategory/Icontomato4733.jpg"
+    Pimg2.src="http://ecoharvest.farm/img/NewCategory/IconBasilica2159.jpg"
+    Pimg3.src="http://ecoharvest.farm/img/NewCategory/IconGarlic460.jpg"
+    Pimg4.src="http://ecoharvest.farm/img/NewCategory/IconDill152.jpg"
+})
+
+Dried_fruits.addEventListener("click",function(){
+    document.querySelectorAll(".variant").forEach(item=>{
+        item.classList.remove("variant1")
+    })
+    this.classList.add("variant1")
+    Pimg1.src="http://ecoharvest.farm/img/NewCategory/IconDried%20apricot218.jpg"
+    Pimg2.src="http://ecoharvest.farm/img/NewCategory/IconNutt2432.jpg"
+    Pimg3.src="http://ecoharvest.farm/img/NewCategory/IconWalnut2552.jpg"
+    Pimg4.src="http://ecoharvest.farm/img/NewCategory/Icondried%20apple3124.jpg"
+})
+
+Dried_vegetables.addEventListener("click",function(){
+    document.querySelectorAll(".variant").forEach(item=>{
+        item.classList.remove("variant1")
+    })
+    this.classList.add("variant1")
+    Pimg1.src="http://ecoharvest.farm/img/NewCategory/Icondried%20spinach3521.jpg"
+    Pimg2.src="http://ecoharvest.farm/img/NewCategory/Icondried%20dill2726.jpg"
+    Pimg3.src="http://ecoharvest.farm/img/NewCategory/Icondried%20mint324.jpg"
+    Pimg4.src="http://ecoharvest.farm/img/NewCategory/Icondried%20parsley2843.jpg"
+})
